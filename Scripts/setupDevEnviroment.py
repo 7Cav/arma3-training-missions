21b72b0a371274d043c9f70a2c62264ddc2ede3f
ColdEvul <andreas.brostrom.ce@gmail.com>
2020-01-29 22:18:17 +0100

    updatred script to allow force and added warnings

--- Scripts/setupDevEnviroment.py
+++ Scripts/setupDevEnviroment.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, os
+import sys, os, shutil
 import argparse
 
 __version__ = 1.0
@@ -10,6 +10,64 @@ scriptPath = os.path.realpath(__file__)
 scriptDir = os.path.dirname(scriptPath)
 rootDir = os.path.dirname(os.path.dirname(scriptPath))
 os.chdir(rootDir)
+
+def request_action(text='Continue?'):
+    Continue_Count = 0
+    while(True):
+        yes_no = input('{} (Yes or No)\n> '.format(text))
+        yes_no = yes_no.lower()
+        if (yes_no == 'yes' or yes_no == 'y'):
+            return True
+        elif (yes_no == 'no' or yes_no == 'n'):
+            return False
+        else:
+            pass
+        Continue_Count += 1
+        if Continue_Count >= 3:
+            sys.exit()
+        print('Write yes (y) or no (n)')
+
+
+def checkForcScripts(path=''):
+    cScriptsControllFiles = [
+        '\\cScripts',
+        '\\cScripts\\script_component.hpp',
+        '\\cScripts\\CavFnc\\cScripts_postInit.sqf',
+        '\\cScripts\\CavFnc\\functions\\script_component.hpp',
+        '\\description.ext',
+        '\\initServer.sqf'
+    ]
+    for c in cScriptsControllFiles:
+        if not os.path.exists('{}{}'.format(path,c)):
+            sys.exit('Error: cScripts could be detected within the given directory.')
+    print('cScripts location is valid...')
+
+
+def checkIfFileExist(path='',force=False):
+    filelist = [
+        'cScripts',
+        'description.ext',
+        'init.sqf',
+        'initServer.sqf',
+        'cba_settings.sqf'
+    ]
+    remove = False
+    for i in filelist:
+        if os.path.exists('{}\\{}'.format(path,i)):
+            if not force:
+                print('{} already exist...'.format(i))
+                if os.path.exists('{}\\setup.json'.format(path)):
+                    remove = request_action('Do you whant to replace the file and continue?')
+                else:
+                    remove = request_action('WARNING: \'setup.json\' does not exist and have not been setup. Continuing may have consiquenses on mission preformence and setup.\nDo you whant to replace the file and continue?')
+            if remove or force:
+                try:
+                    os.remove('{}\\{}'.format(path,i))
+                except:
+                    shutil.rmtree(('{}\\{}'.format(path,i)))
+    else:
+        return False
+
 
 def main():
     # Handle argumets
@@ -22,6 +80,15 @@ def main():
     parser.add_argument('path',
         help='Define the fullpath to the cScripts directory.'
     )
+    parser.add_argument('mission',
+        help='Define mission specific use * for all.'
+    )
+
+    parser.add_argument('--force', '-f',
+        help='Force this will overwrite and ignore all warnings. WARNING: \'setup.json\' does not exist and have not been setup. Continuing may have consiquenses on mission preformence and setup.',
+        action='store_true'
+    )
+
     parser.add_argument('--version', action='version', version='Author: Andreas Broström <andreas.brostrom.ce@gmail.com>\nScript version: {}'.format(__version__))
 
     args = parser.parse_args()
@@ -30,23 +97,17 @@ def main():
     print('Setting up development enviroment...')
 
     # Check if cscripts exist in side of given directory
-    print('Checking cScripts location \'{}\''.format(args.path))
+    print('Looking for cScripts in given location \'{}\''.format(args.path))
+    checkForcScripts(args.path)
 
-    cScriptsControllFiles = [
-        '\\cScripts',
-        '\\cScripts\\script_component.hpp',
-        '\\cScripts\\CavFnc\\cScripts_postInit.sqf',
-        '\\cScripts\\CavFnc\\functions\\script_component.hpp',
-        '\\description.ext',
-        '\\initServer.sqf'
-    ]
-    for c in cScriptsControllFiles:
-        if not os.path.exists('{}{}'.format(args.path,c)):
-            sys.exit('Error: No cScript could be detected within the given directory.')
-    print('cScripts location is valid...')
-
-    # Fetch files and aply blacklist
+    # Fetch files and apply blacklist
     mission_list = os.listdir(rootDir)
+    if not args.mission == "*":
+        if args.mission in mission_list:
+            mission_list = [args.mission]
+        else:
+            sys.exit('There are no mission by that name...')
+
     blacklist_files = [
         '.git',
         '.github',
@@ -70,7 +131,7 @@ def main():
         except:
             pass
     
-    # fetch cScript files and remove blacklisted
+    # fetch cScripts files and remove blacklisted
     content = os.listdir(args.path)
     for f in blacklist_files:
         try:
@@ -83,18 +144,10 @@ def main():
 
         # Check if its a mission
         if not os.path.exists('{}\\{}\\mission.sqm'.format(rootDir,mission)):
-            print('{} is not a mission skipping...'.format(mission))
             continue
 
         #check if mission already have cScripts
-        if os.path.exists('{}\\{}\\cScripts'.format(rootDir,mission)):
-            print('cScripts already installed in this mission skipping...')
-            continue
-        if os.path.exists('{}\\{}\\description.ext'.format(rootDir,mission)):
-            print('description.ext already setup in this mission skipping...')
-            continue
-        if os.path.exists('{}\\{}\\init.sqf'.format(rootDir,mission)):
-            print('init.sqf already setup in this mission skipping...')
+        if checkIfFileExist('{}\\{}'.format(rootDir,mission),args.force):
             continue
 
         for obj in content:
@@ -103,7 +156,7 @@ def main():
                 print('Linking', '{}\\{}'.format(args.path,obj).ljust(62), '<===>'.ljust(8), '{}\\{}\\{}'.format(rootDir,mission,obj))
                 continue
             except:
-                print('Warning: {} already exist for {} skipping...'.format(obj, mission))
+                pass
 
 
 
