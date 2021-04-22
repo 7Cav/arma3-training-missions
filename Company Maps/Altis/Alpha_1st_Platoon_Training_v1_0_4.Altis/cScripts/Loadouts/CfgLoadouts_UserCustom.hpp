f53d7436106abadfb2b302813ef9d52ee2a95590
Micah Whitsel <micah.whitsel@gmail.com>
2021-04-21 22:33:38 -0800

    Alpha 1st PLT Initial Commit

---
+++ Company Maps/Altis/Alpha_1st_Platoon_Training_v1_0_4.Altis/cScripts/Loadouts/CfgLoadouts_UserCustom.hpp
@@ -0,0 +1,22 @@
+/* This file is for mission makers to set up Custom Loadouts for players.
+    
+    Here is a copy paste friendly empty template:
+
+class My_Soldier_Classname_or_VariableName : CommonBlufor {
+    //regiment = "";
+    //company = "";
+
+    //displayName = "";
+    //scope = 0;
+    //category[] = {"cScripts_Loadout_Cat_Other"};
+    //loadout = [[],[],[],[],[],[],"","",[],["","","","","",""]];
+    //insignia = "";
+
+    //abilityMedic = 0;
+    //abilityEngineer = 0;
+    //abilityEOD = 0;
+
+    //preLoadout = "";
+    //postLoadout = "";
+};
+*/
